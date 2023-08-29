9f2472f71594b0c6eb36989c17712911c9b7e0b6
CHARM <CHARM@172.25.13.244>
2023-08-29 16:14:15 +0800

    First java project

---
+++ src/cs4charmcasinillo/CS4CharmCasinillo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs4charmcasinillo;
+
+/**
+ *
+ * @author CHARM
+ */
+public class CS4CharmCasinillo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String greeting = "Hello world.";
+        System.out.println("greeting + Hello Pisay.");
+        System.out.println("Hello world.");
+    }
+    
+}
